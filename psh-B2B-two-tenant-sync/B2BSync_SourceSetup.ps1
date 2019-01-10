e11ed70557574e62f72295f7d8c6c1396003624c
Brett Hacker <brhacker@microsoft.com>
2019-01-10 13:32:33 -0600

    Update B2BSync_SourceSetup.ps1
    Fix bug in B2BSync sourcesetup

--- psh-B2B-two-tenant-sync/B2BSync_SourceSetup.ps1
+++ psh-B2B-two-tenant-sync/B2BSync_SourceSetup.ps1
@@ -7,17 +7,9 @@ See README.txt for details
 # Dot-sourcing variables - update "B2BSync-Myvars.ps1 and use that file name
 . "$PSScriptRoot\B2BSync-MyVars.ps1"
 
-# Load dialog library
-. "$PSScriptRoot\B2BAdminConsentDialog.ps1"
-
 #consent URL (admin loads in browser)
 $consentUrl = "https://login.microsoftonline.com/$($SourceTenantId)/oauth2/authorize?response_type=id_token&client_id=$($appID)&redirect_uri=$appReplyUrl&response_mode=form_post&nonce=a4014117-28aa-47ec-abfb-f377be1d3cf6&resource=https://graph.windows.net&prompt=admin_consent"
-$result = LoadConsentDialog -Url $consentUrl -Title "Admin Consent Required" -ReturnUrl $appReplyUrl
-
-## Show the form, and wait for the response 
-if ($result -imatch "Cancel") {
-    Write-Host "You must consent in order to continue."
-    exit
-}
+start $consentUrl
 
 Write-Host "If you successfully consented, you can now run B2BSync.ps1"
+
